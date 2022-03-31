f1d4c9c5b9528eaee23cc82920f881ff6d25209a
erikaan <ancoraerika@gmail.com>
2022-03-31 16:20:42 +0200

    Aggiungere i file di progetto.

---
+++ HelloCsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information//
+// Un primo programma in C. //
+Console.WriteLine("\nCiao, benvenuto nella mia esercitazione con Visual Studio!\nHo per te un questionario. \nTra questi... ");
+Console.WriteLine("Gennaio");
+Console.WriteLine("Febbraio");
+Console.WriteLine("Marzo");
+Console.WriteLine("Aprile");
+Console.WriteLine("Maggio");
+Console.WriteLine("Giugno");
+Console.WriteLine("Luglio");
+Console.WriteLine("Agosto");
+Console.WriteLine("Settembre");
+Console.WriteLine("Ottobre");
+Console.WriteLine("Novembre");
+Console.WriteLine("Dicembre");
+
+Console.WriteLine("... quale mese preferisci? ");
+var mese = Console.ReadLine();
+
+Console.WriteLine("\a Grazie per aver partecipato! Arrivederci! ");
+
+
+
+
+
